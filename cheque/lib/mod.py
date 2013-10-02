c3c766fbfea3e8ffd8f9d5f66aa922f6680e9c2c
Louis Billiet <louis.billiet59@gmail.com>
2013-10-02 14:48:11 +0200

    ajout d'une bibliotheque pour les calculs modulaires

---
+++ cheque/lib/mod.py
@@ -0,0 +1,15 @@
+def ModularExponantiation(base, exposant, module):
+    return pow(base, exposant, module)
+
+def invMOD(number,module):
+    x=0; y=1;
+    u=1; v=0;
+    a=number; b=module;
+    while (0 != a):
+        q = int(b/a);
+        r = b%a;
+        m = x - u*q;
+        n = y - v*q;
+        b=a; a=r; x=u; y=v; u=m; v=n;
+    return b == 1 and ((x+module)%module) or None 
+
