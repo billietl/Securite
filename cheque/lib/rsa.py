7e514f0486ee953bf561971e7f1727162afb7b08
Louis Billiet <louis.billiet59@gmail.com>
2013-10-02 19:10:07 +0200

    galerade sur le (de)chiffrement de string

--- cheque/lib/rsa.py
+++ cheque/lib/rsa.py
@@ -2,6 +2,7 @@
 
 from prime import *
 from mod import *
+import struct
 
 class NotImplementedYetError(Exception):
     pass
@@ -12,21 +13,55 @@ def generer_cles(taille):
    q = generate_prime(taille/2)
    while p == q:
        q = generate_prime(taille/2)
-   print "p et q confirmes"
    n = p*q
    phi = (p-1)*(q-1)
    e = 3
    while (not pgcd(e,phi) == 1) and (e < phi):
-       print "calcul de e et d"
        e = e+2
-   print "e et d trouves"
    d = invMOD(e, phi)
    return ((n,e),(n,d))
 
+def bourrer(message, chunk=4):
+    nb_pad = chunk - (len(message)%chunk)
+    for i in range(0,nb_pad):
+        message = message + chr(0)
+    return message
+
+def _chiffrer_octet(message, kPub):
+   return pow(message, kPub[1], kPub[0])
+
+def _chiffrer_string(message, kPub, chunk=4):
+    buffer = ''
+    index = 0
+    while index < len(message)
+        i = 0
+        for i in range (0,chunk):
+            # Ajouter un octet au buffer
+        # chiffrer buffer
+        # ajout au buffer general
+    # retour totalite
+
 def chiffrer(message, kPub):
-   '''TODO : chiffrer le message (string) avec kPub'''
-   return message
+   if isinstance(message, (str,unicode)):
+       message = bourrer(message)
+       return _chiffrer_string(message, kPub)
+   if isinstance(message, int):
+       return _chiffrer_octet(message, kPub)
+   raise Exception('Arrete de me passer n\'importe quoi a chiffrer')
+
+def _dechiffrer_octet(message, kPriv):
+   return pow(message, kPriv[1], kPriv[0])
+
+def _dechiffrer_string(message, kPriv, chunk=4):
+   buffer = ''
+   for c in message:
+       # on dechiffre
+       # on decompose
+       # on retourne
 
 def dechiffrer(message, kPriv):
-   '''TODO : dechiffrer le message (string) avec kPriv'''
-   return message
+   if isinstance(message, (str,unicode)):
+       return _dechiffrer_string(message, kPriv)
+   if isinstance(message, int):
+       return _dechiffrer_octet(message, kPriv)
+   raise Exception('Arrete de me passer n\'importe quoi a dechiffrer')
