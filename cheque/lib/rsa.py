bed8c39d5cc1f675418266ea9f9ebefb7b5c9c15
Louis Billiet <louis.billiet59@gmail.com>
2013-10-02 14:40:44 +0200

    definitiondes methodes accessibles via l'API

--- cheque/lib/rsa.py
+++ cheque/lib/rsa.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
+class NotImplementedYetError(Error):
+    pass
+
+def generer_cles(taille)
+   '''TODO : générer une paire de clés et renvoyer le tuple'''
+   raise NotImplementedYetError('Je ne sais pas encore generer les cles !') 
+
 def chiffrer(message, kPub)
-   pass
+   '''TODO : chiffrer le message (string) avec kPub'''
+   return message
 
 def dechiffrer(message, kPriv)
-   pass
+   '''TODO : dechiffrer le message (string) avec kPriv'''
+   return message
