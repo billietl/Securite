098091945f480480dad6d7d11d76491bf6113829
Louis Billiet <louis.billiet59@gmail.com>
2013-10-07 18:45:30 +0200

    ca marche. Enfin, il faut juste debugguer le chiffrement et le dechiffrement...

--- cheque/lib/rsa.py
+++ cheque/lib/rsa.py
@@ -8,7 +8,6 @@ class NotImplementedYetError(Exception):
     pass
 
 def generer_cles(taille):
-   '''TODO : generer une paire de cles et renvoyer le tuple'''
    p = generate_prime(taille/2)
    q = generate_prime(taille/2)
    while p == q:
@@ -21,29 +20,48 @@ def generer_cles(taille):
    d = invMOD(e, phi)
    return ((n,e),(n,d))
 
-def bourrer(message, chunk=4):
+def _bourrer(message, chunk=4):
     nb_pad = chunk - (len(message)%chunk)
-    for i in range(0,nb_pad):
+    for i in xrange(0,nb_pad):
         message = message + chr(0)
     return message
 
 def _chiffrer_octet(message, kPub):
    return pow(message, kPub[1], kPub[0])
 
+def _int_to_string(integer, chunk=4):
+    print integer
+    octet_array = []
+    while integer > 0:
+        octet_array.append(integer & 0xFF)
+        integer >>= 8
+    l = len(octet_array)
+    if chunk >= 1:
+        octet_array = [0]*(chunk-l) + octet_array
+    print "-->" + str(octet_array)
+    return octet_array
+
+def _string_to_int(phrase):
+    print phrase
+    print "-->" + str(int(''.join(['%02x' % i for i in phrase]), 16))
+    return int(''.join(['%02x' % i for i in phrase]), 16)
+
 def _chiffrer_string(message, kPub, chunk=4):
     buffer = ''
     index = 0
-    while index < len(message)
+    while index < len(message):
+        in_buffer = list()
         i = 0
-        for i in range (0,chunk):
-            # Ajouter un octet au buffer
-        # chiffrer buffer
-        # ajout au buffer general
-    # retour totalite
+        for i in xrange(0,chunk):
+            in_buffer.append(ord(message[index+i]))
+        index = index+chunk
+        in_buffer = _string_to_int(in_buffer)
+        buffer = buffer + unichr(_chiffrer_octet(in_buffer, kPub))
+    return buffer
 
 def chiffrer(message, kPub):
    if isinstance(message, (str,unicode)):
-       message = bourrer(message)
+       message = _bourrer(message)
        return _chiffrer_string(message, kPub)
    if isinstance(message, int):
        return _chiffrer_octet(message, kPub)
@@ -55,9 +73,10 @@ def _dechiffrer_octet(message, kPriv):
 def _dechiffrer_string(message, kPriv, chunk=4):
    buffer = ''
    for c in message:
-       # on dechiffre
-       # on decompose
-       # on retourne
+       output = _dechiffrer_octet(ord(c), kPriv)
+       output_buffer = _int_to_string(output)
+       buffer = buffer + str(output_buffer)
+   return buffer
 
 def dechiffrer(message, kPriv):
    if isinstance(message, (str,unicode)):
