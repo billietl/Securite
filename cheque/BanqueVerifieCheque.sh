d9c17073913297f9420f28b2538e5cfcd5647da0
Louis Billiet <louis.billiet59@gmail.com>
2013-10-17 19:53:45 +0200

    traduction de BanqueVerifieCheque

---
+++ cheque/BanqueVerifieCheque.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+usage(){
+    echo $0 " <fichier cheque>"
+    exit 1
+}
+
+clean(){
+    cd ..
+    rm -r $tmpdir
+}
+
+if [ ! $# -eq 1 ]
+then 
+    usage
+fi
+if ! ls $1 >/dev/null
+then
+    usage
+fi
+
+tmpdir=tmp$RANDOM$RANDOM$RANDOM$RANDOM
+mkdir $tmpdir
+cd $tmpdir
+
+# Extraction des fichiers
+tar xzf ../$1 >/dev/null
+tar xzf banque.certif.tgz >/dev/null
+
+# Convertion en variables
+rib=`cat cheque.txt | head -n 1`
+transactionID=`cat cheque.txt | tail -n 1`
+
+# Verification
+transaction=$rib$transactionID
+
+touch ../banque/transactionsFile
+if grep -e $transaction ../banque/transactionsFile >/dev/null
+then
+    echo "Ce chèque a déjà été encaissé !"
+    clean
+    exit 1
+fi
+echo $transaction >> ../banque/transactionsFile
+
+# Nettoyage
+clean
+
+echo "Seems legit."
