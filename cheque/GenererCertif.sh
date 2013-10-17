f346b966249637bf210451f08da9baf11c9a78b7
Louis Billiet <louis.billiet59@gmail.com>
2013-10-17 18:18:43 +0200

    correction de la generation de fichier

--- cheque/GenererCertif.sh
+++ cheque/GenererCertif.sh
@@ -14,19 +14,28 @@ then
     usage
 fi
 
+tmpdir="tmp$RANDOM$RANDOM$RANDOM$RANDOM"
+
 # Creation d'un nouveau RIB
+echo "Creation d'un RIB"
 newrib=$RANDOM
-while ! grep -e $newrib banque/ribsFile >/dev/null
+while grep -e $newrib banque/ribsFile >/dev/null
 do
+    echo "Nope, un autre"
     newrib=$RANDOM
 done
 echo $newrib >> banque/ribsFile
+echo "RIB généré"
 
+mkdir $tmpdir
+cd $tmpdir
 # Creation du fichier
-if [ -e $1/banque.certif.tgz ]; then rm $1/banque.certif.tgz; fi
-echo "$1\n$newrib\n$cle" > banque.certif
-cat banque.certif | openssl dgst -sha256 -sign banque/private.key > banque.certif.sha256
-tar czf $1/banque.certif.tgz banque.certif banque.certif.sha256 $1/public.key
+if [ -e $1/banque.certif.tgz ]; then rm ../$1/banque.certif.tgz; fi
+echo "$1\n$newrib\n$cle" > ./banque.certif
+cp ../$1/public.key .
+cat ./banque.certif | openssl dgst -sha256 -sign ../banque/private.key > ./banque.certif.sha256
+tar czf ../$1/banque.certif.tgz ./banque.certif ./banque.certif.sha256 ./public.key
 
 # Nettoyage
-rm banque.certif banque.certif.sha256
+cd ..
+rm -r $tmpdir
