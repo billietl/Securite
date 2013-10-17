3ecbcaca269df68526c30269d330460f2c316a12
Louis Billiet <louis.billiet59@gmail.com>
2013-10-17 18:36:12 +0200

    Correction dossier temporaire

--- cheque/GenererCertif.sh
+++ cheque/GenererCertif.sh
@@ -15,22 +15,19 @@ then
 fi
 
 tmpdir="tmp$RANDOM$RANDOM$RANDOM$RANDOM"
-
-# Creation d'un nouveau RIB
-echo "Creation d'un RIB"
-newrib=$RANDOM
-while grep -e $newrib banque/ribsFile >/dev/null
-do
-    echo "Nope, un autre"
-    newrib=$RANDOM
-done
-echo $newrib >> banque/ribsFile
-echo "RIB généré"
-
 mkdir $tmpdir
 cd $tmpdir
+
+# Creation d'un nouveau RIB
+newrib=$RANDOM$RANDOM
+while grep -e $newrib ../banque/ribsFile >/dev/null
+do
+    newrib=$RANDOM$RANDOM
+done
+echo $newrib >> ../banque/ribsFile
+
 # Creation du fichier
-if [ -e $1/banque.certif.tgz ]; then rm ../$1/banque.certif.tgz; fi
+if [ -e ../$1/banque.certif.tgz ]; then rm ../$1/banque.certif.tgz; fi
 echo "$1\n$newrib\n$cle" > ./banque.certif
 cp ../$1/public.key .
 cat ./banque.certif | openssl dgst -sha256 -sign ../banque/private.key > ./banque.certif.sha256
